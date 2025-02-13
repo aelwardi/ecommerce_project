e94b84f179c7bbd5392b38e858f71fe96e2c4aad
aelwardi <a.elwardi98@gmail.com>
2025-02-13 16:58:41 +0100

    Sauvegarde les infos dans la bd

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $from = $this->createForm(RegisterUserType::class);
+        $user = new User();
+        $from = $this->createForm(RegisterUserType::class, $user);
+        $from->handleRequest($request);
+        
+        if ($from->isSubmitted() && $from->isValid()) {
+            //dd($from->getData());
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
         return $this->render('register/index.html.twig', [
             'registerForm' => $from->createView()
         ]);
--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,11 +16,36 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('submit', SubmitType::class)
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse email',
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse email'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Choisissez votre mot de passe'
+                ]
+            ])
+            ->add('firstName', \Symfony\Component\Form\Extension\Core\Type\TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre prénom'
+                ]
+            ])
+            ->add('lastName', \Symfony\Component\Form\Extension\Core\Type\TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre nom'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider'
+                , 'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
         ;
     }
 
